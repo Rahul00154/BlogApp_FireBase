7cdcae576cca8bf55ee3a51b1bb0dba9d1605c29
Rahul Anand <duffer.rdx@gmail.com>
2023-12-08 00:49:23 +0530

    fixed deletepost error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,37 @@
 import React, { useState, useEffect } from "react";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-  updateDoc,
-} from "firebase/firestore";
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import { auth, db } from "../config/firebase";
 
 function Home({ isAuth }) {
-  const [postList, setPostList] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const postsCollectionRef = collection(db, "posts"); //collection takes two arguement as first the db and second where you wanna update
 
   //we need postlist when component is loaded so here we use useEffect
+
+  const getPosts = async () => {
+    try {
+      onSnapshot(postsCollectionRef, (snapshot) => {
+        const postList = snapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          };
+        });
+        setPosts(postList);
+        return postList;
+      });
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
+
   useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+    // const getPosts = async () => {
+    //   const data = await getDocs(postsCollectionRef);
+    //   setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    // };
     getPosts();
   }, []);
 
@@ -26,13 +39,14 @@ function Home({ isAuth }) {
     const postDoc = doc(db, "posts", id);
     await deleteDoc(postDoc);
   };
+
   // const updatePost = async (post, id) => {
   //   const postDoc = doc(db, post, id);
   //   await updateDoc(postDoc);
   // };
   return (
     <div className="homePage">
-      {postList.map((post) => {
+      {posts.map((post) => {
         return (
           <div className="post" key={post.id}>
             <div className="postHeader">
